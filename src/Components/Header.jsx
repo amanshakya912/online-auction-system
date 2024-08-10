9398b7c8177c85e174c88953175395f4aaae641e
aman <amanshakya9912@gmail.com>
2024-08-10 06:51:43 +0545

    Tailwind css

---
+++ src/Components/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+    return (
+        <>
+            <div className="absolute w-full">
+                <div className="flex justify-between">
+                    <div>
+                        Logo
+                    </div>
+                    <ul className="flex">
+                        <li>Home</li>
+                        <li>Category</li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Header
---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,15 @@
+import Banner from "../Components/Banner"
+import Footer from "../Components/Footer"
+import Header from "../Components/Header"
+
+const Homepage = () => {
+    return (
+        <>
+            <Header/>
+                <Banner />
+                <div>Hello</div>
+            <Footer/>
+        </>
+    )
+}
+export default Homepage
