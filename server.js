bb5f8a176bfbf2a61991fba89474735b57edd291
aman <amanshakya9912@gmail.com>
2024-11-19 00:01:34 +0545

    setup

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config(); // Load environment variables from .env file
 
 const app = express()
+
+console.log(process.env.MONGO_URI);
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 app.get('/', (req, res) => {
     res.json({mssg: 'welcome'})
