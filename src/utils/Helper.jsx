1ffc09a8b37c99ec14fcfe91f48cfc4394fe079e
aman <amanshakya9912@gmail.com>
2025-01-20 06:45:36 +0545

    helper changee

--- src/utils/Helper.jsx
+++ src/utils/Helper.jsx
@@ -2,10 +2,12 @@
 // const API_URL = "http://localhost:4000/api/";
 // const IMAGE_URL = "http://localhost:4000/uploads/";
 
-const BASE_URL = "https://online-auction-system-1.onrender.com";
-const API_URL = "https://online-auction-system-1.onrender.com/api/";
-const IMAGE_URL = "https://online-auction-system-1.onrender.com/uploads/";
-
+// const BASE_URL = "https://online-auction-system-1.onrender.com";
+// const API_URL = "https://online-auction-system-1.onrender.com/api/";
+// const IMAGE_URL = "https://online-auction-system-1.onrender.com/uploads/";
+const BASE_URL = "https://7d87-36-252-141-122.ngrok-free.app";
+const API_URL = "https://7d87-36-252-141-122.ngrok-free.app/api/";
+const IMAGE_URL = "https://7d87-36-252-141-122.ngrok-free.app/uploads/";
 export default {
     BASE_URL,
     API_URL,
